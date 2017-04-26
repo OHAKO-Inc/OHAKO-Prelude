297cc7e26d64141bcaafa2f336f22d5af179cf12
Yoshikuni Kato <y.kato@ohako-inc.jp>
2017-04-26 18:41:29 +0900

    Update TableView Scroll interface

--- Prelude/Prelude/UIKitExtensions/UITableView+Scroll.swift
+++ Prelude/Prelude/UIKitExtensions/UITableView+Scroll.swift
@@ -10,22 +10,11 @@ import UIKit
 
 public extension UITableView {
 
-    public func scrollToBottom(animated: Bool, viewAnimation: Bool = false) {
+    public func scrollToBottom(animated: Bool) {
         guard numberOfSections > 0 && numberOfRows(inSection: numberOfSections - 1) > 0 else {
             return
         }
         let indexPath = IndexPath(row:  numberOfRows(inSection: numberOfSections - 1)-1, section: numberOfSections - 1)
         self.scrollToRow(at: indexPath, at: .bottom, animated: animated)
-
-        // copied from Dear project
-//        UIView.animate(withDuration: 
-//            viewAnimation ? 0.1 : 0.0,
-//            delay: 0.0,
-//            options: [UIViewAnimationOptions.CurveEaseInOut],
-//            animations: { () -> Void in
-//                self.scrollToRowAtIndexPath(indexPath, atScrollPosition: .Bottom, animated: animated)
-//            },
-//            completion: nil
-//        )
     }
 }
