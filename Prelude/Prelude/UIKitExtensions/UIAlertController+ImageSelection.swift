928961a2fa09ea64b77997751cb792a13d3e17a5
Yoshikuni Kato <y.kato@ohako-inc.jp>
2017-04-26 18:41:16 +0900

    Update ImageSelection interface

--- Prelude/Prelude/UIKitExtensions/UIAlertController+ImageSelection.swift
+++ Prelude/Prelude/UIKitExtensions/UIAlertController+ImageSelection.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 public extension UIAlertController {
-    public class func showImageSelectAlertController(_ containerVC: UIViewController, imagePicker: UIImagePickerController) {
+    public class func showImageSelectAlertController(container: UIViewController, imagePicker: UIImagePickerController) {
 
         let pickerAlert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
 
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             pickerAlert.addAction(UIAlertAction(title: "アルバムから選択", style: .default, handler: { (_) -> Void in
                 imagePicker.sourceType = .photoLibrary
-                containerVC.present(imagePicker, animated: true, completion: nil)
+                container.present(imagePicker, animated: true, completion: nil)
             }))
         }
 
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             pickerAlert.addAction(UIAlertAction(title: "写真を撮る", style: .default, handler: { (_) -> Void in
                 imagePicker.sourceType = .camera
-                containerVC.present(imagePicker, animated: true, completion: nil)
+                container.present(imagePicker, animated: true, completion: nil)
             }))
         }
 
         pickerAlert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
-        containerVC.present(pickerAlert, animated: true, completion: nil)
+        container.present(pickerAlert, animated: true, completion: nil)
     }
 }
