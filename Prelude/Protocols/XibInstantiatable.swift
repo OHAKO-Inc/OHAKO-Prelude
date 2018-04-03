70f5e09a0a777d7dd8a543ad2bf36edb254603f9
Masakazu Sano <sano@strobolight.co.jp>
2018-04-03 18:08:38 +0900

    Tweak view state in XibInstantiatable

--- Prelude/Protocols/XibInstantiatable.swift
+++ Prelude/Protocols/XibInstantiatable.swift
@@ -10,14 +10,20 @@ import UIKit
 
 public protocol XibInstantiatable {
     func instantiate()
+    func instantiate(isUserInteractionEnabled: Bool)
 }
 
 public extension XibInstantiatable where Self: UIView {
     func instantiate() {
+        instantiate(isUserInteractionEnabled: true)
+    }
+
+    func instantiate(isUserInteractionEnabled: Bool) {
         let nib = UINib(nibName: String(describing: type(of: self)), bundle: Bundle(for: type(of: self)))
         guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
             return
         }
+        view.isUserInteractionEnabled = isUserInteractionEnabled
         addSubview(view)
 
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -27,4 +33,19 @@ public extension XibInstantiatable where Self: UIView {
         view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         view.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
+
+//    func instantiate() {
+//        let nib = UINib(nibName: String(describing: type(of: self)), bundle: Bundle(for: type(of: self)))
+//        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+//            return
+//        }
+//        addSubview(view)
+//
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//
+//        view.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+//        view.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+//        view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+//        view.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+//    }
 }
