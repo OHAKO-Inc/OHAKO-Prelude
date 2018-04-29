9b67c093fc8d1c48929f8e6f5c6d725c6dca2c57
kz56cd <kz56cd@gmail.com>
2018-04-29 23:30:11 +0900

    Clean code

--- Prelude/Protocols/XibInstantiatable.swift
+++ Prelude/Protocols/XibInstantiatable.swift
@@ -33,19 +33,4 @@ public extension XibInstantiatable where Self: UIView {
         view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         view.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
-
-//    func instantiate() {
-//        let nib = UINib(nibName: String(describing: type(of: self)), bundle: Bundle(for: type(of: self)))
-//        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
-//            return
-//        }
-//        addSubview(view)
-//
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//
-//        view.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-//        view.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-//        view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-//        view.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-//    }
 }
